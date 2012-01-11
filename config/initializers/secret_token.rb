06779c41c41c6af403d031f780f64b6297607291
Faye <electro.fairy@gmail.com>
2012-01-11 23:43:56 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StupidApp::Application.config.secret_token = '8c6df130bad958d87551218de885fdedc569986cf9cb9dee6c17df97532b47c78b008c874ef6b62f98b495ce1df7a57fb9ce2e9eab58f379b59bad6541062e42'
